57f1d8265721dad85dc9d600e0338e15f715cbb7
RobinOfiONIZUKA <87935445+RobinOfiONIZUKA@users.noreply.github.com>
2022-03-25 03:47:22 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace Template_Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox24 = new System.Windows.Forms.PictureBox();
+            this.pictureBox22 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox24)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox22)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.AutoRoundedCorners = true;
+            this.guna2Button1.BorderRadius = 35;
+            this.guna2Button1.CheckedState.Parent = this.guna2Button1;
+            this.guna2Button1.CustomImages.Parent = this.guna2Button1;
+            this.guna2Button1.FillColor = System.Drawing.Color.White;
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.Black;
+            this.guna2Button1.HoverState.Parent = this.guna2Button1;
+            this.guna2Button1.Location = new System.Drawing.Point(51, 100);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.ShadowDecoration.Parent = this.guna2Button1;
+            this.guna2Button1.Size = new System.Drawing.Size(231, 72);
+            this.guna2Button1.TabIndex = 0;
+            this.guna2Button1.Text = "Start";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click_1);
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.Animated = true;
+            this.guna2Button2.AutoRoundedCorners = true;
+            this.guna2Button2.BorderRadius = 35;
+            this.guna2Button2.CheckedState.Parent = this.guna2Button2;
+            this.guna2Button2.CustomImages.Parent = this.guna2Button2;
+            this.guna2Button2.FillColor = System.Drawing.Color.White;
+            this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button2.ForeColor = System.Drawing.Color.Black;
+            this.guna2Button2.HoverState.Parent = this.guna2Button2;
+            this.guna2Button2.Location = new System.Drawing.Point(398, 100);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.ShadowDecoration.Parent = this.guna2Button2;
+            this.guna2Button2.Size = new System.Drawing.Size(214, 72);
+            this.guna2Button2.TabIndex = 1;
+            this.guna2Button2.Text = "Verify";
+            this.guna2Button2.Click += new System.EventHandler(this.guna2Button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(183, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(311, 30);
+            this.label1.TabIndex = 243;
+            this.label1.Text = "Server Private Launcher Sourced";
+            // 
+            // pictureBox24
+            // 
+            this.pictureBox24.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox24.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox24.BackgroundImage")));
+            this.pictureBox24.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox24.Location = new System.Drawing.Point(608, 4);
+            this.pictureBox24.Name = "pictureBox24";
+            this.pictureBox24.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox24.TabIndex = 244;
+            this.pictureBox24.TabStop = false;
+            this.pictureBox24.Click += new System.EventHandler(this.pictureBox24_Click_1);
+            // 
+            // pictureBox22
+            // 
+            this.pictureBox22.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox22.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox22.BackgroundImage")));
+            this.pictureBox22.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox22.Location = new System.Drawing.Point(571, 4);
+            this.pictureBox22.Name = "pictureBox22";
+            this.pictureBox22.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox22.TabIndex = 245;
+            this.pictureBox22.TabStop = false;
+            this.pictureBox22.Click += new System.EventHandler(this.pictureBox22_Click);
+            // 
+            // Form1
+            // 
+            this.AccentColor = System.Drawing.Color.Black;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 226);
+            this.Controls.Add(this.pictureBox22);
+            this.Controls.Add(this.pictureBox24);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.guna2Button1);
+            this.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.State = MetroSuite.MetroForm.FormState.Custom;
+            this.Style = MetroSuite.Design.Style.Dark;
+            this.Text = "Server Private Launcher Sourced";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox24)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox22)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox24;
+        private System.Windows.Forms.PictureBox pictureBox22;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Template_Launcher
+{
+    public partial class Form1 : MetroSuite.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+
+        private async void guna2Button1_Click_1(object sender, EventArgs e)
+        {
+            string[] FNStuff = { "FortniteClient-Win64-Shipping_EAC.exe", "FortniteClient-Win64-Shipping_BE.exe", "FortniteLauncher.exe" };
+
+            foreach (string procname in FNStuff)
+            {
+                var process = Process.GetProcessesByName(procname);
+                foreach (var proc in process)
+                {
+                    proc.Kill();
+                }
+            }
+            string TempPath = Path.GetTempPath();
+            var Path1 = "";
+            var version = "1";
+
+            var path1 = File.ReadAllText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Epic\\UnrealEngineLauncher\\LauncherInstalled.dat"));
+            dynamic Json = JsonConvert.DeserializeObject(path1);
+
+            foreach (var installion in Json.InstallationList)
+            {
+                if (installion.AppName == "Fortnite")
+                {
+                    Path1 = installion.InstallLocation.ToString() + "\\FortniteGame\\Binaries\\Win64";
+                    version = installion.AppVersion.ToString().Split('-')[1];
+                }
+            }
+
+
+            if (!File.Exists(Path1 + "\\FortniteClient-Win64-Shipping_EAC.old")) { }
+            else
+            {
+                File.Move(Path1 + "\\FortniteClient-Win64-Shipping_EAC.exe", Path1 + "\\FortniteClient-Win64-Shipping_EAC.exe.old");
+            }
+
+            if (!File.Exists(Path1 + "\\FortniteClient-Win64-Shipping_BE.old")) { }
+            else
+            {
+                File.Move(Path1 + "\\FortniteClient-Win64-Shipping_BE.exe", Path1 + "\\FortniteClient-Win64-Shipping_BE.exe.old");
+            }
+
+            WebClient webClient = new WebClient();
+
+            await webClient.DownloadFileTaskAsync("anti ac link", TempPath + "\\FortniteClient-Win64-Shipping_EAC.exe");
+            await webClient.DownloadFileTaskAsync("anti ac link", TempPath + "\\FortniteClient-Win64-Shipping_BE.exe");
+            if (!File.Exists(TempPath + "\\dllname.dll"))
+            {
+                await webClient.DownloadFileTaskAsync("dll link", TempPath + "\\dllname.dll");
+            }
+
+            if (!File.Exists(Path1 + "\\FortniteClient-Win64-Shipping_EAC.exe"))
+            {
+                File.Move(TempPath + "\\FortniteClient-Win64-Shipping_EAC.exe", Path1 + "\\FortniteClient-Win64-Shipping_EAC.exe");
+            }
+            else
+            {
+                File.Delete(Path1 + "\\FortniteClient-Win64-Shipping_EAC.exe");
+                File.Move(TempPath + "\\FortniteClient-Win64-Shipping_EAC.exe", Path1 + "\\FortniteClient-Win64-Shipping_EAC.exe");
+            }
+
+            if (!File.Exists(Path1 + "\\FortniteClient-Win64-Shipping_BE.exe"))
+            {
+                File.Move(TempPath + "\\FortniteClient-Win64-Shipping_BE.exe", Path1 + "\\FortniteClient-Win64-Shipping_BE.exe");
+            }
+            else
+            {
+                File.Delete(Path1 + "\\FortniteClient-Win64-Shipping_BE.exe");
+                File.Move(TempPath + "\\FortniteClient-Win64-Shipping_BE.exe", Path1 + "\\FortniteClient-Win64-Shipping_BE.exe");
+            }
+
+            if (!File.Exists(Path1 + "\\dllname.dll"))
+            {
+                File.Move(TempPath + "\\dllname", Path1 + "\\dllname.dll");
+            }
+            else
+            {
+                File.Delete(Path1 + "\\dllname.dll");
+                File.Move(TempPath + "\\dllname.dll", Path1 + "\\dllname.dll");
+                var Proc = new ProcessStartInfo();
+                Proc.CreateNoWindow = true;
+                Proc.FileName = "cmd.exe";
+                Proc.Arguments = "/C start com.epicgames.launcher://apps/Fortnite?action=launch";
+                Process.Start(Proc);
+
+            }
+        }
+
+        private void pictureBox24_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox24_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox22_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            var Proc = new ProcessStartInfo();
+            Proc.CreateNoWindow = true;
+            Proc.FileName = "cmd.exe";
+            Proc.Arguments = "/C start com.epicgames.launcher://apps/Fortnite?action=verify";
+        }
+    }
+}
+
+
+
+
